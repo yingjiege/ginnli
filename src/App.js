cf47e382221d1eb84ca23347037dfd63980424df
yingjiege <1026509513ge@gmail.com>
2024-07-01 16:09:12 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'; // Example page component
-import ProductList from './components/ProductList'; // Example component
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Use 'element' prop instead of 'component' */}
           {/* Add more routes as needed */}
         </Routes>
       </div>
