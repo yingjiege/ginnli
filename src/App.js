6577d9e1249412167fb3baaaa096a9bda6218cc7
yingjiege <127132937+yingjiege@users.noreply.github.com>
2024-07-01 13:57:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter  as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'; // Example page component
 import ProductList from './components/ProductList'; // Example component
 
